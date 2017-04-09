cf6d704283629a0b6f0646c3345c4f1a10dd4652
Bakterija <atiskr@gmail.com>
2017-04-09 17:34:35 +0300

    Added accurate top, down scrolling with cursor in TextEditor

--- src/app_modules/widgets/text_editor.py
+++ src/app_modules/widgets/text_editor.py
@@ -2,7 +2,7 @@ from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
 from kivy.lang import Builder
-from kivy.clock import Clock
+from kivy.clock import Clock, mainthread
 
 
 Builder.load_string('''
@@ -19,6 +19,8 @@ Builder.load_string('''
             font_size: logview_font_size
             on_cursor: root.on_cursor(args[1])
             on_cursor_row: root.on_cursor_row(args[1])
+            cursor_width: cm(0.1)
+            cursor_color: 0.2, 0.4, 0.7, 1.0
 ''')
 
 
@@ -29,11 +31,34 @@ class TextEditor(BoxLayout):
     def on_cursor(self, value):
         pass
 
-    def on_cursor_row(self, value):
-        max_lines = int(self.ids.input.height / self.ids.input.line_height)
-        prc = value / max_lines
-        # print (value, max_lines, prc)
-        self.ids.scroller.scroll_y = 1.0 - prc
+    def on_cursor_row(self, row):
+        scroll_y = self.ids.scroller.scroll_y
+        negscroll_y = 1.0 - self.ids.scroller.scroll_y
+
+        inp_height = self.ids.input.height
+        scrl_height = self.ids.scroller.height
+        size1 = inp_height - scrl_height
+
+        line_height = self.ids.input.line_height
+        max_lines = int(inp_height / line_height) - 1
+        rem_lines = int(size1 / line_height)
+        mrem_lines = (max_lines - (max_lines - rem_lines))
+
+        visible_first = int(mrem_lines * negscroll_y)
+        visible_last = int(visible_first + scrl_height / line_height)
+        if visible_last > max_lines:
+            visible_last = max_lines
+
+        prc = 0.0
+        if row > 0.0 and max_lines > 0.0:
+            prc = row / mrem_lines
+
+        conv_lh = self.ids.scroller.convert_distance_to_scroll(
+            0, scrl_height - line_height * 1.2)
+        if row < visible_first or row == 0:
+            self.ids.scroller.scroll_y = 1.0 - prc
+        elif row > visible_last or row == max_lines:
+            self.ids.scroller.scroll_y = 1.0 - prc + conv_lh[1]
 
 
 class TextEditorPopup(Popup):
